c005748b1ef71b58c8c14a81011ab653901d29b3
Adderstorm <leonidp2@mail.ru>
2023-05-11 20:47:49 +0600

    Added logging functionality and basic token input

---
+++ SquadBot_Application/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Microsoft.AspNetCore.Mvc;
+using SquadBot_Application.Logging;
+
+namespace SquadBot_Application.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        [HttpPost]
+        public ActionResult PostToken(string token)
+        {
+            try
+            {
+                TokenUtils.ValidateToken(TokenType.Bot, token);
+            }
+            catch 
+            {
+                Logger.LogError("Discord Bot token validation error");
+                return ValidationProblem("Discord Bot token is invalid, please check it and try send again");
+            }
+            return Ok();
+        }
+
+    }
+}
---
+++ SquadBot_Application/Logging/LogType.cs
@@ -0,0 +1,9 @@
+﻿namespace SquadBot_Application.Logging
+{
+    public enum LogType
+    {
+        Error,
+        Warning,
+        Info
+    }
+}
