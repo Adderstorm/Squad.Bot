cf72d671fa39d18acb838d37e632c81731b3c8a9
Adderstorm <leonidp2@mail.ru>
2023-12-12 19:26:21 +0600

    Finished event OnUserStateUpdate, and written func's for almost every actions for private rooms

--- Squad.Bot/ComponentsInteraction/PrivateRoomsComponents.cs
+++ Squad.Bot/ComponentsInteraction/PrivateRoomsComponents.cs
@@ -45,60 +45,30 @@ namespace Squad.Bot.ComponentsInteraction
                 Title = "Portals was successfully deleted",
                 Description = "Now you can use again /private_rooms invite",
                 Color = CustomColors.Success,
-            }.WithAuthor(name: Context.Guild.CurrentUser.Nickname, iconUrl: Context.Guild.CurrentUser.GetGuildAvatarUrl());
+            }.WithAuthor(name: Context.Guild.CurrentUser.Nickname ?? Context.User.Username ?? Context.User.GlobalName, iconUrl: Context.Guild.CurrentUser.GetGuildAvatarUrl());
 
-            await ModifyOriginalResponseAsync(x => { x.Content = null; x.Embed = embed.Build(); });
+            await RespondAsync(embed: embed.Build(), ephemeral: true);
         }
 
-        // TODO: Change the stubs with the working code
-        [ComponentInteraction("portal.rename:*")]
-        public async Task Rename(string channelName)
-        {
-            var savedPortal = await _dbContext.PrivateRooms.FirstOrDefaultAsync(x => x.Guilds.Id == Context.Guild.Id);
-
-            var user = Context.Guild.GetUser(Context.User.Id);
-
-            if (IsUserInPRoom(Context, user))
-            {
-                // TODO: add user owner check
-                await user.VoiceChannel.ModifyAsync(x => x.Name = channelName);
-
-                var embed = new EmbedBuilder
-                {
-                    Title = "Channel was successfully renamed",
-                    Color = CustomColors.Success,
-                };
-
-                await RespondAsync(embed: embed.Build(), ephemeral: true);
-            }
-            else
-            {
-                var embed = new EmbedBuilder
-                {
-                    Title = "Oooppss, something went wrong...",
-                    Description = "This could be due to the fact that you are not in a private room",
-                    Color = CustomColors.Failure,
-                };
-
-                await RespondAsync(embed: embed.Build(), ephemeral: true);
-            }
-        }
-
+        // TODO: Bug: the bot revokes all channel rights when they are redefined
+        // TODO: Change the stubs with the working code(need to learn Modals)
         [ComponentInteraction("portal.hide")]
         public async Task Hide()
         {
             var user = Context.Guild.GetUser(Context.User.Id);
 
             // TODO: Fill embeds with information
-            if (IsUserInPRoom(Context, user))
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
             {
-                if(user.VoiceChannel.PermissionOverwrites.FirstOrDefault().Permissions.ViewChannel == PermValue.Allow || user.VoiceChannel.PermissionOverwrites.FirstOrDefault().Permissions.ViewChannel == PermValue.Inherit)
+                var userChannelPermissions = user.VoiceChannel.PermissionOverwrites.FirstOrDefault().Permissions;
+
+                if(userChannelPermissions.ViewChannel == PermValue.Allow || userChannelPermissions.ViewChannel == PermValue.Inherit)
                 {
                     ulong everyoneRoleId = Context.Guild.Roles.First(x => x.Name == "@everyone").Id;
 
                     var voiceOverwrites = new PermissionOverwriteHelper(everyoneRoleId, PermissionTarget.Role)
                     {
-                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(viewChannel: PermValue.Deny, connect: PermValue.Deny)
+                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(viewChannel: PermValue.Deny)
                     };
                     await user.VoiceChannel.ModifyAsync(tsp => { tsp.PermissionOverwrites = voiceOverwrites.CreateOverwrites(); });
                     
@@ -115,14 +85,14 @@ namespace Squad.Bot.ComponentsInteraction
                 {
                     var voiceOverwrites = new PermissionOverwriteHelper(Context.User.Id, PermissionTarget.User)
                     {
-                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(viewChannel: PermValue.Allow, connect: PermValue.Deny)
+                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(viewChannel: PermValue.Allow)
                     };
                     await user.VoiceChannel.ModifyAsync(tsp => { tsp.PermissionOverwrites = voiceOverwrites.CreateOverwrites(); });
 
                     EmbedBuilder embed = new()
                     {
                         Title = "Show/Hide room for everyone",
-                        Description = $"{Context.Guild.CurrentUser.Nickname}, ",
+                        Description = $"{Context.Guild.CurrentUser.Nickname ?? Context.User.Username ?? Context.User.GlobalName}, ",
                         Color = CustomColors.Success
                     };
 
@@ -134,48 +104,31 @@ namespace Squad.Bot.ComponentsInteraction
                 var embed = new EmbedBuilder
                 {
                     Title = "Oooppss, something went wrong...",
-                    Description = "This could be due to the fact that you are not in a private room",
+                    Description = "This could be due to the fact that you are not in a private room or not the owner",
                     Color = CustomColors.Failure,
                 };
 
                 await RespondAsync(embed: embed.Build(), ephemeral: true);
             }
         }
-
-        [ComponentInteraction("portal.kick:*")]
-        public async Task Kick(IUser userToKick)
+        
+        [ComponentInteraction("portal.kick")]
+        public async Task Kick()
         {
-            await Logger.LogInfo($"kick {userToKick}");
-
             var user = Context.Guild.GetUser(Context.User.Id);
 
-            if (IsUserInPRoom(Context, user))
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
             {
-
+                
             }
         }
 
-        [ComponentInteraction("portal.limit:*")]
-        public async Task Limit(ushort limit)
+        [ComponentInteraction("portal.owner")]
+        public async Task Owner()
         {
-            await Logger.LogInfo($"limit {limit}");
-
             var user = Context.Guild.GetUser(Context.User.Id);
 
-            if (IsUserInPRoom(Context, user))
-            {
-
-            }
-        }
-
-        [ComponentInteraction("portal.owner:*")]
-        public async Task Owner(IUser newOwner)
-        {
-            await Logger.LogInfo($"owner {newOwner}");
-
-            var user = Context.Guild.GetUser(Context.User.Id);
-
-            if (IsUserInPRoom(Context, user))
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
             {
 
             }
@@ -184,14 +137,137 @@ namespace Squad.Bot.ComponentsInteraction
         [ComponentInteraction("portal.lock")]
         public async Task Lock()
         {
-            await Logger.LogInfo($"lock");
+            var user = Context.Guild.GetUser(Context.User.Id);
+
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
+            {
+                var userChannelPermissions = user.VoiceChannel.PermissionOverwrites.FirstOrDefault().Permissions;
+
+                if (userChannelPermissions.Connect == PermValue.Allow || userChannelPermissions.Connect == PermValue.Inherit)
+                {
+                    ulong everyoneRoleId = Context.Guild.Roles.First(x => x.Name == "@everyone").Id;
+
+                    var voiceOverwrites = new PermissionOverwriteHelper(everyoneRoleId, PermissionTarget.Role)
+                    {
+                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(connect: PermValue.Deny)
+                    };
+                    await user.VoiceChannel.ModifyAsync(tsp => { tsp.PermissionOverwrites = voiceOverwrites.CreateOverwrites(); });
+
+                    EmbedBuilder embed = new()
+                    {
+                        Title = "Lock/unlock room for everyone",
+                        Description = "",
+                        Color = CustomColors.Success
+                    };
+
+                    await RespondAsync(embed: embed.Build(), ephemeral: true);
+                }
+                else
+                {
+                    ulong everyoneRoleId = Context.Guild.Roles.First(x => x.Name == "@everyone").Id;
+
+                    var voiceOverwrites = new PermissionOverwriteHelper(everyoneRoleId, PermissionTarget.Role)
+                    {
+                        Permissions = PermissionOverwriteHelper.SetOverwritePermissions(connect: PermValue.Allow)
+                    };
+                    await user.VoiceChannel.ModifyAsync(tsp => { tsp.PermissionOverwrites = voiceOverwrites.CreateOverwrites(); });
+
+                    EmbedBuilder embed = new()
+                    {
+                        Title = "Lock/unlock room for everyone",
+                        Description = "",
+                        Color = CustomColors.Success
+                    };
+
+                    await RespondAsync(embed: embed.Build(), ephemeral: true);
+                }
+            }
+            else
+            {
+                var embed = new EmbedBuilder
+                {
+                    Title = "Oooppss, something went wrong...",
+                    Description = "This could be due to the fact that you are not in a private room or not the owner",
+                    Color = CustomColors.Failure,
+                };
+
+                await RespondAsync(embed: embed.Build(), ephemeral: true);
+            }
+        }
+
+        [ComponentInteraction("portal.limit")]
+        public async Task Limit()
+        {
+            var user = Context.Guild.GetUser(Context.User.Id);
+
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
+            {
+                await RespondWithModalAsync<LimitModal>("changeLimit");
+            }
+            else
+            {
+                var embed = new EmbedBuilder
+                {
+                    Title = "Oooppss, something went wrong...",
+                    Description = "This could be due to the fact that you are not in a private room or not the owner",
+                    Color = CustomColors.Failure,
+                };
+
+                await RespondAsync(embed: embed.Build(), ephemeral: true);
+            }
+        }
+
+        [ComponentInteraction("portal.rename")]
+        public async Task Rename()
+        {
+            var savedPortal = await _dbContext.PrivateRooms.FirstOrDefaultAsync(x => x.Guilds.Id == Context.Guild.Id);
 
             var user = Context.Guild.GetUser(Context.User.Id);
 
-            if (IsUserInPRoom(Context, user))
+            if (IsUserInPRoom(Context, user) && IsUserOwner(user))
             {
-
+                await RespondWithModalAsync<RenameModal>("RenameModal");
             }
+            else
+            {
+                var embed = new EmbedBuilder
+                {
+                    Title = "Oooppss, something went wrong...",
+                    Description = "This could be due to the fact that you are not in a private room or not the owner",
+                    Color = CustomColors.Failure,
+                };
+
+                await RespondAsync(embed: embed.Build(), ephemeral: true);
+            }
+        }
+
+        [ModalInteraction("renameModal")]
+        public async Task RenameModalInteraction(RenameModal modal)
+        {
+
+            await Context.Guild.CurrentUser.VoiceChannel.ModifyAsync(x => x.Name = modal.ChannelName);
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Channel was successfully renamed",
+                Color = CustomColors.Success,
+            };
+
+            await RespondAsync(embed: embed.Build(), ephemeral: true);
+        }
+
+        [ModalInteraction("changeLimit")]
+        public async Task ChangeLimitInteraction(LimitModal modal)
+        {
+            await Context.Guild.CurrentUser.VoiceChannel.ModifyAsync(x => x.UserLimit = modal.Limit);
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Limit was successfully changed",
+                Color = CustomColors.Success,
+            };
+
+            await RespondAsync(embed: embed.Build(), ephemeral: true);
         }
         private bool IsUserInPRoom(SocketInteractionContext context, SocketGuildUser user)
         {
@@ -201,6 +277,34 @@ namespace Squad.Bot.ComponentsInteraction
                 return true;
             else
                 return false;
+        }
+        private static bool IsUserOwner(SocketGuildUser user)
+        {
+            var permissions = user.VoiceChannel.GetPermissionOverwrite(user);
+            if (permissions != null && permissions.Value.ManageChannel == PermValue.Allow)
+                return true;
+            else
+                return false;
+        }
+
+        public class RenameModal : IModal
+        {
+            public string Title => "Rename channel";
+
+            [RequiredInput]
+            [InputLabel("New name")]
+            [ModalTextInput("rename.new_name", style: TextInputStyle.Paragraph, placeholder: "You can use {game}, {username} or any other text name you want", maxLength: 100)]
+            public string ChannelName { get; set; } = null!;
+        }
+
+        public class LimitModal : IModal
+        {
+            public string Title => "Change channel limit";
+
+            [RequiredInput]
+            [InputLabel("New limit")]
+            [ModalTextInput("limit.new_limit", maxLength: 2)]
+            public int Limit { get; set; }
         }
     }
 }
--- Squad.Bot/Events/OnUserStateChange.cs
+++ Squad.Bot/Events/OnUserStateChange.cs
@@ -25,7 +25,7 @@ namespace Squad.Bot.Events
             await CollectData(user, oldState, newState);
         }
 
-        // TODO: Change the stubs with the working code and !!! delete static field in CollectData
+        // TODO: Change the stub with the working code and !!!(if need) delete static field in CollectData
         private static async Task CollectData(SocketUser user, SocketVoiceState oldState, SocketVoiceState newState)
         {
             await Logger.LogInfo($"{user}, {newState}, {oldState}");
@@ -38,17 +38,25 @@ namespace Squad.Bot.Events
                 savedPortal = await _dbContext.PrivateRooms.FirstOrDefaultAsync(x => x.Guilds.Id == newState.VoiceChannel.Guild.Id);
             else if(newState.VoiceChannel == null)
                 savedPortal = await _dbContext.PrivateRooms.FirstOrDefaultAsync(x => x.Guilds.Id == oldState.VoiceChannel.Guild.Id);
-
-
-            if (newState.VoiceChannel != null && newState.VoiceChannel.Id == savedPortal.ChannelID)
+            else
+                savedPortal = await _dbContext.PrivateRooms.FirstOrDefaultAsync(x => x.Guilds.Id == newState.VoiceChannel.Guild.Id);
+            
+            if (newState.VoiceChannel != null && oldState.VoiceChannel != null && newState.VoiceChannel.Id == savedPortal.ChannelID && oldState.VoiceChannel.Category.Id == savedPortal.CategoryID && IsUserOwner(oldState, user))
+            {
+                var member = newState.VoiceChannel.Guild.GetUser(user.Id);
+                await member.ModifyAsync(x => x.Channel = oldState.VoiceChannel);
+                return;
+            }
+            else if (newState.VoiceChannel != null && newState.VoiceChannel.Id == savedPortal.ChannelID)
             {
                 var permissions = new PermissionOverwriteHelper(user.Id, PermissionTarget.User)
                 {
                     Permissions = PermissionOverwriteHelper.SetOverwritePermissions(muteMembers: PermValue.Allow,
                                                                                     deafenMembers: PermValue.Allow,
-                                                                                    manageChannel: PermValue.Deny)
+                                                                                    manageChannel: PermValue.Allow)
                 };
-                var newVoiceChannel = await newState.VoiceChannel.Guild.CreateVoiceChannelAsync($"{user.Username}'s channel", tcp => {
+                var guildUser = newState.VoiceChannel.Guild.GetUser(user.Id);
+                var newVoiceChannel = await newState.VoiceChannel.Guild.CreateVoiceChannelAsync($"{guildUser.Nickname ?? user.GlobalName ?? user.Username}'s channel", tcp => {
                                                                                                                                          tcp.CategoryId = savedPortal.CategoryID;
                                                                                                                                          tcp.PermissionOverwrites = permissions.CreateOverwrites();
                                                                                                                                      });
@@ -64,6 +72,15 @@ namespace Squad.Bot.Events
                     await oldState.VoiceChannel.DeleteAsync();                    
                 }
             }
+        }
+
+        private static bool IsUserOwner(SocketVoiceState state, SocketUser user)
+        {
+            var permissions = state.VoiceChannel.GetPermissionOverwrite(user);
+            if (permissions != null && permissions.Value.ManageChannel == PermValue.Allow)
+                return true;
+            else
+                return false;
         }
     }
 }
